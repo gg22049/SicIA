c5b4ceb0c084d8b9da2ae6fb0c3cfa957c79a918
gg22049 <gg22049@ues.edu.sv>
2025-03-10 15:58:49 -0600

    linpieza

---
+++ backend.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# df = pd.read_csv('data_raw.csv')
+# df.dropna(axis=0, how='all', inplace=True)
+# df["prcp"] = df["prcp"].fillna(0)
+# df.to_csv('datos_limpios.csv', index=False)
